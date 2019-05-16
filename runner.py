05d099621de9d1939112c3994fe8246deda739ad
Jussi Löppönen <jilopponen@gmail.com>
2019-05-16 23:13:52 +0300

    ckpt path back

--- runner.py
+++ runner.py
@@ -30,7 +30,7 @@ from get_data import get_embedding_matrix
 class RunNMT:
     def __init__(self, languages, Tx, Ty, num_layers, units, batch_size, work_dir, lr=0.001, beam_width=1):
         assert tf.test.is_gpu_available(), "This application requires GPU to run."
-        self.checkpoint_dir = 'checkpoints/{}-{}'.format(languages[0], languages[1])
+        self.checkpoint_dir = os.path.abspath('{}/{}-{}-checkpoints'.format(work_dir, languages[0], languages[1]))
         self.checkpoint_path = os.path.join(self.checkpoint_dir, "model.ckpt")
         self.weights_dir = 'weights/{}-{}'.format(languages[0], languages[1])
         self.weights_path = '{}/model.ckpt'.format(self.weights_dir)
